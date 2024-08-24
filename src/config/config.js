d1e19b9bec4f42219f975fd2dd9f757c7ec6fb29
Priyanshu Soni <devpriyanshu001@gmail.com>
2024-08-24 15:35:34 +0530

    update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,4 +4,5 @@ export const config = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
 };
